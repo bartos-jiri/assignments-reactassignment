79d18ae8261da48efd0c3c35542f1b70f37a80d2
Jiří Bartoš <bartosjir@gmail.com>
2023-06-25 13:21:02 +0300

    F6: Implemented Footer items count functionality

--- client/src/TodoFooter.tsx
+++ client/src/TodoFooter.tsx
@@ -1,5 +1,14 @@
+import { useQuery } from "react-query";
 import { Footer } from "./components/Footer";
+import { getTodos } from "./api";
 
 export const TodoFooter: React.FC = () => {
-    return <Footer />;
+    const { data: counts } = useQuery("todos", getTodos, {
+        select: (todos) => ({
+            todo: todos.filter((t) => !t.done).length,
+            done: todos.filter((t) => t.done).length,
+        }),
+    });
+
+    return <Footer doneItems={counts?.done} todoItems={counts?.todo} />;
 };
