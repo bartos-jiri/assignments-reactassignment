387a52ce3ec14f4890a4b219a1a69475ea08652e
Jiří Bartoš <bartosjir@gmail.com>
2023-06-25 18:02:52 +0300

    B1: Input with ref, adjusted visuals

--- client/src/components/form/Input.tsx
+++ client/src/components/form/Input.tsx
@@ -2,13 +2,26 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { InputProps } from "./types";
 
-const StyledInput = styled.input``;
+const StyledInput = styled.input`
+    border-radius: 9999px;
+    padding: 0 12px;
+    border: 1px solid;
+    border-color: ${(props) => props.theme.colors.olive6};
+    outline: none;
 
-export const Input = (props: InputProps): JSX.Element => {
+    :focus-visible,
+    :focus,
+    :focus-within {
+        border-color: ${(props) => props.theme.colors.olive10};
+    }
+`;
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>((props: InputProps, ref): JSX.Element => {
     const [value, setValue] = useState(props.initialValue);
 
     return (
         <StyledInput
+            ref={ref}
             value={value}
             onChange={(e) => {
                 const value = e.currentTarget.value;
@@ -17,4 +30,4 @@ export const Input = (props: InputProps): JSX.Element => {
             }}
         />
     );
-};
+});
