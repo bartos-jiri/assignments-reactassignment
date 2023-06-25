bbee6d5bac6079260e6081e51d45b7b643a86fd2
Jiří Bartoš <bartosjir@gmail.com>
2023-06-25 18:15:40 +0300

    B2: Added padding to List

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
+    padding: 10px 20px;
 `;
 
 export const List: React.FC<PropsWithChildren> = ({ children }) => <StyledDiv>{children}</StyledDiv>;
