5e981d71cbd62d59bf4e0bcf11c6d1fdd9a75636
Jiří Bartoš <bartosjir@gmail.com>
2023-06-25 13:15:16 +0300

    F5: Sorted todos

--- client/src/TodoList.tsx
+++ client/src/TodoList.tsx
@@ -4,7 +4,15 @@ import { getTodos } from "./api";
 import { TodoListItem } from "./TodoListItem";
 
 export const TodoList: React.FC = () => {
-    const { data: todos, isLoading } = useQuery("todos", getTodos);
+    const { data: todos, isLoading } = useQuery("todos", getTodos, {
+        select: (todos) => {
+            todos.sort(
+                (a, b) =>
+                    Number(a.done) - Number(b.done) || new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+            );
+            return todos;
+        },
+    });
 
     if (isLoading || !todos) {
         return null;
